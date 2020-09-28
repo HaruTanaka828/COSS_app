65c9d1fa2ea7f24b2e71d8a5e17ec9133e94d59c
vagrant <vagrant@localhost.localdomain>
2020-09-28 13:24:09 +0000

    add

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,6 +5,6 @@ class Blog < ApplicationRecord
   attachment :image
 
   validates :title, presence: true, length: {minimum: 1, maximum: 15}
-  validates :body, presence: true, length: {minimum: 10, maximum: 200}
+  validates :body, presence: true, length: {minimum: 20, maximum: 200}
 
 end
